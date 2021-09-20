dd9945141f0ca68e9af160b03dfd9f37272542f4
theolivenbaum <rafael@rafael.aero>
2021-09-20 08:32:45 +0200

    Add code to reproduce bug under net6.0

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using Aspose.Pdf;
+Console.WriteLine("Hello, World!");
+
+new License().SetLicense("Aspose.PDF.NET.lic"); //This call crashes on net6.0
+
+using var doc = new Document("sample.pdf");
+Console.WriteLine($"This pdf has {doc.Pages.Count} pages");
